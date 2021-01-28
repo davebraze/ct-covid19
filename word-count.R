d0ab88562621693f4e20d86ffbf395445b4c596c
Dave Braze <davebraze@gmail.com>
2021-01-28 17:17:45 -0500

    doodling with word counts for the web page

---
+++ word-count.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(tidytext)
+
+lines <- read_lines("ct-covid.Rmd")
+
+text <- tibble(line = lines) %>%
+    unnest_tokens(word, line) %>%
+    anti_join(stop_words)
+
+## need to exclude yaml block and R code chunks
+
+text %>%
+    count(word, sort=TRUE) %>%
+    head(20)
+
