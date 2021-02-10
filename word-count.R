c4ae633002ebea70920c59ce54bd8df57660831e
Dave Braze <davebraze@gmail.com>
2021-02-10 11:29:37 -0500

    doodling with word count

--- word-count.R
+++ word-count.R
@@ -1,15 +1,15 @@
 library(tidyverse)
 library(tidytext)
+library(stringr)
 
 lines <- read_lines("ct-covid.Rmd")
 
 text <- tibble(line = lines) %>%
-
-
+    mutate (line = str_replace_all(line, "[Cc]ovid-19", "covid19")) %>%
     unnest_tokens(word, line) %>%
     anti_join(stop_words)
 
-## need to exclude yaml block and R code chunks
+## need to exclude yaml block, R code chunks, inline code, links, & references
 
 text %>%
     count(word, sort=TRUE) %>%
